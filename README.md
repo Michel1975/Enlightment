23bf7bdd4d93f192dd0041c0c1ea900ce61bd1de
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-07-24 09:25:56 +0200

    Initial setup with Bootstrap template

---
+++ README.md
@@ -0,0 +1,2 @@
+Prototype application version 0.1 - goal is 0.5 for initial release
+by [Michel Kenneth Hansen](http://michelhansen75@hotmail.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enlightment::Application.config.secret_token = '057931e69879a700e6fc0707492d816c81fa9584a2cce036844b387f2b5553f19e1f22d9e1d1a616434a91efbaa15152fac721f00e0354595a0bdbb5e43dbbf4'
